f1740fabedd9dd3f6d54b8c234f074d65c8b4d9d
Muhamed Kapo <evolt@Evolts-MBP.localdomain>
2022-03-21 16:34:06 +0100

    added realistic examples

--- examples/main.tf
+++ examples/main.tf
@@ -1,19 +1,19 @@
 locals {
-  created_groups = merge(module.gitlab-group-module-root.created_groups, module.gitlab-group-module-lvl1.created_groups)
+  created_groups = merge(module.gitlab-group-module-developers.created_groups, module.gitlab-group-module-frontend.created_groups)
 }
 
-module "gitlab-group-module-root" {
+module "gitlab-group-module-developers" {
   source = "../gitlab-group-module"
   groups = {
-    root_group = {
+    developers = {
       auto_devops_enabled               = false
       default_branch_protection         = 2
       emails_disabled                   = false
       lfs_enabled                       = true
       mentions_disabled                 = false
-      name                              = "dev"
+      name                              = "developers"
       parent_id                         = 0
-      path                              = "dev2249"
+      path                              = "dev2284"
       project_creation_level            = "developer"
       request_access_enabled            = true
       require_two_factor_authentication = false
@@ -25,13 +25,13 @@ module "gitlab-group-module-root" {
   }
 }
 
-module "gitlab-group-module-lvl1" {
+module "gitlab-group-module-frontend" {
   source = "../gitlab-group-module"
   groups = {
-    ennioGroup1 = {
-      name      = "ennioGroup1"
-      path      = "ennioGroup1path"
-      parent_id = module.gitlab-group-module-root.created_groups["root_group"].id
+    react = {
+      name      = "React"
+      path      = "react"
+      parent_id = module.gitlab-group-module-developers.created_groups["developers"].id
     }
   }
 }
@@ -39,9 +39,9 @@ module "gitlab-group-module-lvl1" {
 module "gitlab-project-module" {
   source = "../gitlab-project-module"
   projects = {
-    react_test_project = {
-      name         = "React test project"
-      namespace_id = local.created_groups["ennioGroup1"].id
+    react_example_project = {
+      name         = "React example project"
+      namespace_id = local.created_groups["react"].id
       push_rules = {
         commit_committer_check = true
       }
@@ -54,42 +54,40 @@ module "gitlab-user-module" {
   groups   = local.created_groups
   projects = module.gitlab-project-module.created_projects
   users = {
-    voodoo000 = {
+    johnharper = {
       create   = false
-      username = "voodoo000"
-      email    = "narit35022@snece.com"
-      name     = "Voodoo Voodoo"
+      username = "john.harper"
+      email    = "john.harper@example.com"
+      name     = "John Harper"
       groups = {
-        ennioGroup1 = {
+        react = {
           access_level = "maintainer"
           expires_at   = null
         }
-        root_group = {
+        developers = {
           access_level = "guest"
         }
       }
     }
-    voodoo111 = {
-      username = "voodoo111"
-      email    = "nayan85537@superyp.com"
-      name     = "Voodoo Voodoo"
+    chrisharper = {
+      username = "chris.harper"
+      email    = "chris.harper@example.com"
+      name     = "Chris Harper"
       groups = {
-        ennioGroup1 = {
+        react = {
           access_level = "developer"
           expires_at   = "2030-12-31"
         }
-        root_group = {
+        developers = {
           access_level = "guest"
           expires_at   = "2030-12-31"
         }
       }
       projects = {
-        react_test_project = {
+        react_example_project = {
           access_level = "maintainer"
         }
       }
     }
   }
 }
-
-# push real examples so clients can easly understand module
